bb28dc4dab3943d62a7cfbb4db1c9db8a95ebe0e
Samuel Fisch <samuelzfisch@gmail.com>
2026-02-15 09:16:20 -0500

    Add root route

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
+app.get("/", (req, res) => {
+  res.status(200).send("JumpScared backend is running ✅");
+});
+
 
 const PORT = process.env.PORT || 3000;
 
